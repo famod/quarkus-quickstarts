189580a33a5a215536e2f8c5f75ac905eb0d1104
Falko Modler <famod@users.noreply.github.com>
2020-09-14 22:33:21 +0200

    Demonstrate issue #12087

--- getting-started-testing/src/test/java/org/acme/getting/started/testing/GreetingServiceTest.java
+++ getting-started-testing/src/test/java/org/acme/getting/started/testing/GreetingServiceTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 
 @QuarkusTest
+@TestProfile(GreetingServiceTest.SomeProfile.class)
 public class GreetingServiceTest {
 
     @Inject
@@ -16,5 +19,9 @@ public class GreetingServiceTest {
     @Test
     public void testGreetingService() {
         Assertions.assertEquals("hello Quarkus", service.greeting("Quarkus"));
+    }
+
+    public static class SomeProfile implements QuarkusTestProfile {
+
     }
 }
